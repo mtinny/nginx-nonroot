871f61ef8501ed047aa5912da49c069cbb9d8c37
Masakazu Tinny Taniguchi <mc-ns-controller-for-oc-portal@mail.rakuten.com>
2019-04-25 19:53:36 +0900

    tag to latest

--- build-push.sh
+++ build-push.sh
@@ -4,6 +4,9 @@ repo=mtinny/nginx-toolbox
 tag=${tag:-v0.0.1}
 
 image="${repo}:${tag}"
+latest="${repo}:latest"
 docker build -t ${image} .
 docker push ${image}
 
+docker tag ${image} ${latest}
+docker push ${latest}
